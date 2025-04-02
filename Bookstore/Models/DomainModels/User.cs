7cfb7c2f84bf630d7642e9ea20c15feff161a222
Luke Hamann <135341515+luke-hamann@users.noreply.github.com>
2025-04-02 11:10:30 -0500

    Add step 20.

--- Bookstore/Models/DomainModels/User.cs
+++ Bookstore/Models/DomainModels/User.cs
@@ -6,6 +6,9 @@ namespace Bookstore.Models
 {
     public class User : IdentityUser
     {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+
         [NotMapped]
         public IList<string> RoleNames { get; set; }
     }
